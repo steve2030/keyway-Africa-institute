c83b1738bd1b976ba9de9a341700d0ca5a13d7eb
steve2030 <stevemighty2030@gmil.com>
2023-05-21 20:58:20 +0300

    initialized a college website

--- assets/vendor/typed.js/typed.js
+++ assets/vendor/typed.js/typed.js
@@ -1,10 +1,6 @@
 /*!
  * 
- *   typed.js - A JavaScript Typing Animation Library
- *   Author: Matt Boldt <me@mattboldt.com>
- *   Version: v2.0.12
- *   Url: https://github.com/mattboldt/typed.js
- *   License(s): MIT
+ *   
  * 
  */
 (function webpackUniversalModuleDefinition(root, factory) {
